8f54abc658e2205223a294bba45a8691b4055f1c
cloudsony999 <cloudsony999@gmail.com>
2024-07-31 20:04:30 +0530

    ccc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tutorial';
+  users=[
+    {id:11,name:'Rishin'},
+    {id:22,name:'Deblina'},
+    {id:33,name:'Oindrila'},
+    {id:44,name:'Twisha'}
+  ];
+
+  em=[];
+  type:number=10;
+
+  v:number=10;
 }
